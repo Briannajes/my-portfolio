8e5a4514d1fe28647843e5af9c71531d6ffaac9b
Brianna Jesionowski <144929073+Briannajes@users.noreply.github.com>
2026-04-25 14:47:24 -0400

    Update bio text for clarity and grammar

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ function About() {
     <div className="about-container">
       <h1>About Me </h1>
       <p className="bio">
-        Hi! I'm Brianna Jesionowski — a graduating with my Bachleors of Digital Media  at the University of Central Florida in a few short weeks. I have always had a passion for content, social media and the internet which has led me to learning web design, graphic design and content creation. My goal after graduation is to become a creative freelancer in which I will be able to manage my own photography, graphic design, web design and content creation.
+        Hi! I'm Brianna Jesionowski — a soon to be graduate with my Bachleors of Digital Media at the University of Central Florida. I have always had a passion for content, social media and the internet which has led me to learning web design, graphic design and content creation. My goal after graduation is to become a creative freelancer in which I will be able to manage my own photography, graphic design, web design and content creation.
       </p>
 
       <div className="about-sections">
